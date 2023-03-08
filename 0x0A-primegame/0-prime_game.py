285d768703b3d00bd1d5420cef1057e0d4938a08
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2023-03-08 18:36:35 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,56 @@
-#!/usr/bin/env fish
-python3 -m doctest -v (basename (status -f))
-exit
-"""
->>> isWinner = __import__('0-prime_game').isWinner
->>> print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(3, [4, 5, 1])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(0, [4, 3])))
-Winner: None
->>> print("Winner: {}".format(isWinner(5, [1, 2, 3, 4, 5])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(10, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
-Winner: Maria
->>> print("Winner: {}".format(isWinner(4, [11, 30, 1, 7])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(6, [1, 1, 0, 0, 1, 8])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(1, [1])))
-Winner: Ben
->>> print("Winner: {}".format(isWinner(0, [0])))
-Winner: None
->>> print("Winner: {}".format(isWinner(-1, [10])))
-Winner: None
->>> nums = [0] * 100
->>> for i in range(100):
-...     nums[i] = i * i
->>> print("Winner: {}".format(isWinner(100, nums)))
-Winner: Ben
->>> nums = [0] * 10000
->>> for i in range(10000):
-...     nums[i] = i
->>> print("Winner: {}".format(isWinner(10000, nums)))
-Winner: Maria
-"""
+#!/usr/bin/python3
+
+""" Prime Game Algorithm Python """
+
+
+def is_prime(n):
+    """ Checks if a number given n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def calculate_primes(n, primes):
+    """ Calculate all primes """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
+
+def isWinner(x, nums):
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
+
+    players_wins = {"Maria": 0, "Ben": 0}
+
+    primes = [0, 0, 2]
+
+    calculate_primes(max(nums), primes)
+
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
+
+        if (sum_options % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
+
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
+        return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
+    return None
